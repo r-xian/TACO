141c482bc10a3ca8687791db4b2f7e572e1fb9ee
rxian2 <rodxiang2@gmail.com>
2024-08-10 11:35:34 +1000

    configs for running 1m steps for quadruped_run

--- run_quadrun.sh
+++ run_quadrun.sh
@@ -2,21 +2,25 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=20G
-#SBATCH --job-name=cartpole_TACO
+#SBATCH --mem=30G
+#SBATCH --job-name=reacher_TACO
 #SBATCH --gres=gpu:nvidia_a100_80gb_pcie_1g.10gb
 #SBATCH --time=72:00:00
 #SBATCH --partition=gpu_cuda
 #SBATCH --account=a_lead
-#SBATCH -o cartpole.out
-#SBATCH -e cartpole.error
+#SBATCH -o quadrun.out
+#SBATCH -e quadrun.error
 
 module load miniconda3
 source $EBROOTMINICONDA3/etc/profile.d/conda.sh
 conda activate taco
 cd /home/s4642506/TACO
 
-srun python ./train.py agent=taco task=cartpole_swingup seed=1 exp_name=cartpole_1
+srun python ./train.py agent=taco task=quadruped_run seed=1 exp_name=quadruped_run_1 
+srun python ./train.py agent=taco task=quadruped_run seed=1 exp_name=quadruped_run_2
+
+
+
 
 # task=finger_spin
 # task=reacher_easy
