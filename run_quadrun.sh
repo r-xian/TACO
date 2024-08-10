157beaf27421f9d47619b13c573b2698c5c1e932
Rodger Xiang <rodxiang2@gmail.com>
2024-08-10 14:08:41 +1000

    updated quadruped run configs

--- run_quadrun.sh
+++ run_quadrun.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=30G
 #SBATCH --job-name=reacher_TACO
 #SBATCH --gres=gpu:nvidia_a100_80gb_pcie_1g.10gb
-#SBATCH --time=72:00:00
+#SBATCH --time=96:00:00
 #SBATCH --partition=gpu_cuda
 #SBATCH --account=a_lead
 #SBATCH -o quadrun.out
@@ -16,8 +16,8 @@ source $EBROOTMINICONDA3/etc/profile.d/conda.sh
 conda activate taco
 cd /home/s4642506/TACO
 
-srun python ./train.py agent=taco task=quadruped_run seed=1 exp_name=quadruped_run_1 
-srun python ./train.py agent=taco task=quadruped_run seed=1 exp_name=quadruped_run_2
+srun python ./train.py agent=taco task=quadruped_run seed=2 exp_name=quadruped_run_2 
+srun python ./train.py agent=taco task=quadruped_run seed=3 exp_name=quadruped_run_3
 
 
 
